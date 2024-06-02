829baaea1727af4c258f145954085666e196e9e7
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2024-06-02 15:16:20 +0300

    Delete copy files.

--- src copy/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const URL = {
-  // CARS: "http://localhost:5170/api/carsData.json",
-  CARS: "https://kusmiivasyl.github.io/api-luxury-cars/carsData.json"
-};
--- src copy/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import 'react-toastify/dist/ReactToastify.css'
-import "react-datepicker/dist/react-datepicker.css";
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
